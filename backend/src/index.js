b2eedbe238517d30b3899a67a9fb6689e9389d5c
Nsuku Mathebula <mathebulansuku@gmail.com>
2025-04-07 18:20:26 +0200

    adding logic to signing up users

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,8 @@ import { connectDB } from "./lib/db.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json()); // Middleware that allows server to access values from client-side
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,9 +1,41 @@
 import express from "express";
+import User from "../models/User.js";
 
 const router = express.Router();
 
 router.post("/login", async (req, res) => {
   try {
+    const { email, username, password } = req.body;
+
+    if (!username || !password || !email) {
+      return res.status(400).json({ error: "All fields are required" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ error: "Password must be at least 6 characters" });
+    }
+
+    if (username.length < 3) {
+      return res
+        .status(400)
+        .json({ error: "Username must be at least 3 characters" });
+    }
+
+    //check if user already exists
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ error: "Email already exists" });
+    }
+    const existingUsername = await User.findOne({ username });
+    if (existingUsername) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
+
+    const profileImage = `https://api.dicebear.com/7.x/avataaars/svg?seed=${username}`;
+
+    const user = new User({ email, username, password, profileImage });
   } catch (err) {
     console.log(err);
   }
