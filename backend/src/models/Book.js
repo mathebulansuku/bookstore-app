6d29eef5c5b320389e1d66f041c26ad41ae4715e
Nsuku Mathebula <mathebulansuku@gmail.com>
2025-04-08 09:38:25 +0200

    Upload image to cloudinary and get url

--- backend/src/models/Book.js
+++ backend/src/models/Book.js
@@ -29,19 +29,6 @@ const bookSchema = new mongoose.Schema(
       ref: "User",
       required: true,
     },
-
-    author: {
-      type: String,
-      required: true,
-    },
-    publishYear: {
-      type: Number,
-      required: true,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    },
   },
   { timestamps: true }
 );
--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -1,9 +1,18 @@
 import express from "express";
+import cloudinary from "./lib/cloudinary.js";
 
 const router = express.Router();
 
 router.post("/", async (req, res) => {
   try {
+    const { title, caption, rating, image } = req.body;
+    if (!title || !caption || !rating || !image) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    //upload image to cloudinary
+    const uploadResponse = await cloudinary.uploader.upload(image);
+    const imageUrl = uploadResponse.secure_url;
   } catch (error) {}
 });
 
